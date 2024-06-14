d12070a0f7b365e4b6a75f253d36030f914d710a
Gifftybabe <ulokangozi@gmail.com>
2024-06-14 01:36:34 +0100

    HomePage UI

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -3,6 +3,7 @@ import Navbar from "./Navbar";
 import { albumsData } from "../assets/assets";
 import AlbumItem from "./AlbumItem";
 import { songsData } from "../assets/assets";
+import SongItem from "./SongItem";
 
 const DisplayHome = () => {
   return (
@@ -24,10 +25,10 @@ const DisplayHome = () => {
       </div>
 
       <div className="mb-4">
-        <h1 className="my-5 font-bold text-2xl">Today's Biggest Hits/h1>
+        <h1 className="my-5 font-bold text-2xl">Today's Biggest Hits</h1>
         <div className="flex overflow-auto">
-          {albumsData.map((item, index) => (
-            <AlbumItem
+          {songsData.map((item, index) => (
+            <SongItem
               key={index}
               name={item.name}
               desc={item.desc}
